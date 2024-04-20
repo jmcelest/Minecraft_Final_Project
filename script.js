fe8b053b135e1ce7796468551d828fb55b69eb96
Pranav Vijay <pranavvijay@pranavsair2024.myfiosgateway.com>
2024-04-19 23:02:07 -0400

    Added 2 servers to server status page and added two functions in Javascript file to retrieve server status from API

--- script.js
+++ script.js
@@ -18,8 +18,27 @@ function includeFooter() {
     .catch(error => console.error('Error fetching footer:', error));
 }
 
+function getServerStatus(server, element) {
+    fetch("https://api.mcsrvstat.us/3/" + server)
+    .then((res) => res.json())
+    .then((res) => {
+        if(res.online == true) {
+            document.getElementById(element).innerHTML += "Online";
+        }
+        else {
+            document.getElementById(element).innerHTML += "Offline";
+        }
+    });
+}
+
+function getServerInfo() {
+    getServerStatus("mc.hypixel.net", "firstServer");
+    getServerStatus("play.aesthetiful.com", "secondServer");
+}
+
 // Call the functions when the page loads
 window.onload = function() {
     includeNavbar();
     includeFooter();
+    getServerInfo();
 };
