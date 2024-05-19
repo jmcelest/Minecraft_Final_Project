ce304c88c8a14e3fbe6b042a02072a5ff9003d25
Hyelim Seo <hseo1@terpmail.umd.edu>
2024-05-19 18:16:41 -0400

    a

--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ function includeNavbar() {
 
 // Function to include the footer content
 function includeFooter() {
-    fetch('footer.html')
+    fetch('Footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-container').innerHTML = data;
