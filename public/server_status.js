527c7fb01449f9287c8cdf2d43ce789f7bddc929
Hyelim Seo <hseo1@terpmail.umd.edu>
2024-05-19 19:55:38 -0400

    a

--- public/server_status.js
+++ public/server_status.js
@@ -15,6 +15,7 @@ function checkValidation(server) {
     fetch("https://api.mcsrvstat.us/3/" + server)
     .then((res) => res.json())
     .then((res) => {
+        console.log(res);
         if(res.ip == "127.0.0.1") {
             alert("The IP Address is Invalid");
             return false;
